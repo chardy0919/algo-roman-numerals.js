1086b0b719961d49c7caf4403ac754f4692c1d3e
Cody Hardy <chardy0919@gmail.com>
2023-10-04 16:34:04 -0400

    Cody, Basic framework for toRomanLazy function

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,5 +1,11 @@
 function toRomanLazy(num) {
-  return "";
+ let answer = '';
+ let numerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+ if (num > 1000){
+  Math.floor(num/1000)
+  answer+= 
+  num = num%1000;
+ }
 }
 
 function toRoman(num) {
@@ -7,3 +13,11 @@ function toRoman(num) {
 }
 
 module.exports = { toRoman, toRomanLazy };
+
+// I -> 1
+// V -> 5
+// X -> 10
+// L -> 50
+// C -> 100
+// D -> 500
+// M -> 1000
